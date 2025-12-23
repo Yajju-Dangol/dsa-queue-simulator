a22aa545b05adb9460493c510b1fcbfae5f8a4a4
Yajju-Dangol <yazu.dangol@gmail.com>
2025-12-23 21:27:41 +0545

    increased the time for new car generations

--- src/trafficgenerator.cpp
+++ src/trafficgenerator.cpp
@@ -82,7 +82,7 @@ int main()
     }
     std::cout << "Sent lane: " << buffer << std::endl;
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::this_thread::sleep_for(std::chrono::seconds(2));
   }
 
   closesocket(sock);
